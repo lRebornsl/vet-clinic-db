f9037e7ad5713849f5cabcdd31e70487f510f321
lRebornsl <anthony_vasquez98@hotmail.com>
2023-08-15 14:13:16 -0500

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id Int GENERATED ALWAYS AS IDENTITY,
+    name varchar(50),
+    date_of_birth Date,
+    escape_attemps Int,
+    neutered boolean,
+    weight_kg decimal(4,2)
 );
