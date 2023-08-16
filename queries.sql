d04c78c5e83041003c76f313838aeb3101a2a5b6
lRebornsl <anthony_vasquez98@hotmail.com>
2023-08-16 14:08:35 -0500

    Add more queries

--- queries.sql
+++ queries.sql
@@ -38,3 +38,12 @@ UPDATE animals
 SET weight_kg = weight_kg * -1
 WHERE weight_kg < 0;
 COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attemps = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(*) FROM animals GROUP BY neutered;
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals WHERE species = 'digimon';
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals WHERE species = 'pokemon';
+SELECT AVG(escape_attemps) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' AND species = 'digimon';
+SELECT AVG(escape_attemps) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' AND species = 'pokemon';
