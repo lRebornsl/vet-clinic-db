4eeefee6cf80b5cb28c88aa8e2330d8039ffa50c
lRebornsl <anthony_vasquez98@hotmail.com>
2023-08-16 13:31:39 -0500

    Second commit

--- queries.sql
+++ queries.sql
@@ -26,3 +26,15 @@ COMMIT;
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT first;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO first;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
